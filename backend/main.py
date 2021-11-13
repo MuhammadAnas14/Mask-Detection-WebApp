6d4102f20a7028c857eee89d8616ce43c1ec3920
MuhammadAnas14 <anasm9877@gmail.com>
2021-11-13 17:35:56 +0500

    flask initiated

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
-from flask import Flask, request, render_template,jsonify
+from flask import Flask, request, render_template,jsonify ,Response
 app = Flask(__name__)
 import json
 import numpy as np
 from pymongo import MongoClient
 from flask_cors import CORS
 CORS(app)
+from video import VideoCamera
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
 
 @app.route("/", methods=["GET"])
@@ -12,4 +20,14 @@ def Gettingstarted():
     data= "hi"
     print("all ok")
     return data
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=5000, threaded=True, use_reloader=False)
+        app.run(debug=True, port = 5050)
 
